ecefd89041c30f8e9714e11cb8d4de6fca610568
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-06-11 01:48:44 +0600

    selected classes api done

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // MongoDb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oattrlg.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -33,6 +54,9 @@ async function run() {
       .db("anuDesign")
       .collection("instructors");
     const usersCollection = client.db("anuDesign").collection("users");
+    const selectedClassCollection = client
+      .db("anuDesign")
+      .collection("selectedClasses");
 
     // jwt
     app.post("/jwt", (req, res) => {
@@ -43,7 +67,7 @@ async function run() {
 
       res.send({ token });
     });
-
+    // open api
     app.get("/class", async (req, res) => {
       const result = await classCollection.find().toArray();
       res.send(result);
@@ -64,6 +88,30 @@ async function run() {
       }
 
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // selected classes
+    app.post("/selectedClasses", async (req, res) => {
+      const item = req.body;
+      const result = await selectedClassCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.get("/selectedClasses", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "provident access" });
+      }
+      const query = { email: email };
+      const result = await selectedClassCollection.find(query).toArray();
       res.send(result);
     });
 
